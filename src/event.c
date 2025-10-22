3024e257f6eb1b217afa678215501e1bb892076b
CrhistianMRe <came.29meca@gmail.com>
2025-10-22 19:56:12 +0800

    feat: add limited range date validation

--- src/event.c
+++ src/event.c
@@ -12,11 +12,26 @@ void *assignEventValue(char *origin) {
     return result;
 }
 
-_Bool isInvalidDate(char date[10]) {
+//local method
+_Bool isInvalidDateRange(char date[11]) {
+    char monthStr[3], dayStr[3];
+    
+    strncpy(monthStr, &date[5], 2);
+    strncpy(dayStr, &date[8], 2);
+    
+    int month = atoi(monthStr);
+    int day = atoi(dayStr);
+    
+    if (month > 12 || day > 31) return 1;
+    return 0;
+}
+
+_Bool isInvalidDate(char date[11]) {
     int count = 0;
     for (int i = 0; i < 10; i++) {
         if(isdigit(date[i])  || (date[i] == '-')) count ++;
     }
+    if(isInvalidDateRange(date)) count = 99;
     return count != 10;
 }
 
